9bfec4a580ccbb6f58b65e385c28b1d44ae4aa1a
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-09-12 23:08:12 -0300

    feat: create function for placing the marks

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ const gameBoard = (function () {
     const boardSide = 3;
     const board = Array.from({ length: boardSide }, () => Array(boardSide).fill(null));
 
-    return { board };
+    const placeMark = (row, column, player) => {
+        board[row-1][column-1] = player.mark;
+        return board;
+    }
+
+    return { placeMark, board };
 
 })();
 
