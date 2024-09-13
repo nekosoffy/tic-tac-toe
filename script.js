e83a6715bf13c9aad819df216b871615f3eaa196
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-09-13 20:25:04 -0300

    feat: create function for checking win condition

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const gameBoard = (function () {
         console.log(`${player.name} marked row ${row} and column ${column}...`)
         board[row-1][column-1] = player.marker;
         playerManager.switchPlayerTurn();
-        return getBoard();
+        console.table(getBoard());
     };
 
     return { placeMark, getBoard, resetBoard };
@@ -53,7 +53,69 @@ const playerManager = (function () {
 
     const getScore = (i) => players[i]["score"];
     const addScore = (i) => players[i]["score"]++;
+    const getPlayerName = (i) => players[i]["name"];
 
     return { switchPlayerTurn, getActivePlayer, 
-        setPlayerNames, addScore, getScore };
+        setPlayerNames, addScore, getScore, getPlayerName };
 })();
+
+
+const gameController = (function () {
+
+    const checkWin = () => {
+        let board = gameBoard.getBoard();
+        const winPatterns = 
+        [
+        // Horizontal checks
+        [board[0][0], board[0][1], board[0][2]],
+        [board[1][0], board[1][1], board[1][2]],
+        [board[2][0], board[2][1], board[2][2]],
+
+        // Vertical checks
+        [board[0][0], board[1][0], board[2][0]],
+        [board[0][1], board[1][1], board[2][1]],
+        [board[0][2], board[1][2], board[2][2]],
+
+        //Diagonal checks
+        [board[0][0], board[1][1], board[2][2]],
+        [board[0][2], board[1][1], board[2][0]],
+        ]
+
+        const equality = (array1, array2) => {
+            if (array1.length !== array2.length) {
+                return false;
+            }
+            for (let i = 0; i < array1.length; i++) {
+                if (array1[i] !== array2[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        const patternX = (element) => equality(element,["X", "X", "X"]);
+        const patternO = (element) => equality(element,["O", "O", "O"]);
+
+        const xWin = winPatterns.some(patternX);
+        const oWin = winPatterns.some(patternO);
+    
+        if (xWin || oWin) {
+            gameBoard.resetBoard();
+            console.log("Round finished!");
+    
+            if (xWin) {
+                console.log(`${playerManager.getPlayerName(0)} won!`);
+                playerManager.addScore(0);
+            } else if (oWin) {
+                console.log(`${playerManager.getPlayerName(1)} won!`);
+                playerManager.addScore(1);
+            }
+    
+            console.log(`Player 1 score: ${playerManager.getScore(0)}`);
+            console.log(`Player 2 score: ${playerManager.getScore(1)}`);
+        }
+    }
+
+    return { checkWin }
+})();
+
