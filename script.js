bc5d02295c622a9428a2efc5e2a9f34085b55335
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-09-13 10:17:15 -0300

    feat: make the board a private variable

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ const gameBoard = (function () {
     const boardSide = 3;
     const board = Array.from({ length: boardSide }, () => Array(boardSide).fill(null));
 
+    const getBoard = () => board;
+
     const placeMark = (row, column, player) => {
         board[row-1][column-1] = player.mark;
         return board;
     }
 
-    return { placeMark, board };
+    return { placeMark, getBoard };
 
 })();
 
-const Player = (function () {
+const player = (function () {
     const create = (name, mark) => {
         return {
             playerName: name,
